d4634e0024fbc83b759e7e43c7d437a29947fdd7
Rakesh Ramakrishnan <rrakesh711@gmail.com>
2019-04-12 14:05:42 -0700

    Updated forest model

--- __init__.py
+++ __init__.py
@@ -69,7 +69,13 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 clf = DecisionTreeClassifier()
 clf.fit(X_train, y_train)
 y_pred = clf.predict(X_test)
-print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
+print("Decision Tree Accuracy:", metrics.accuracy_score(y_test, y_pred))
+
+# Random Forest Model
+rfc = RandomForestClassifier(n_estimators=10)
+rfc = rfc.fit(X_train, y_train)
+y_pred = rfc.predict(X_test)
+print("Random Forest Accuracy:", metrics.accuracy_score(y_test, y_pred))
 
 # View the decision tree
 pydot = StringIO()
@@ -80,17 +86,3 @@ graph.write_png('DecisionTree.png')
 Image(graph.create_png())
 print("Decision Tree Created")
 
-# Random Forest Model
-clf = RandomForestClassifier()
-clf.fit(X_train, y_train)
-y_pred = clf.predict(X_test)
-print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
-
-# View the decision tree
-pydot = StringIO()
-export_graphviz(clf, out_file=pydot, filled=True, rounded=True, special_characters=True,
-                feature_names=features)
-graph = pydotplus.graph_from_dot_data(pydot.getvalue())
-graph.write_png('RandomForestDecisionTree.png')
-Image(graph.create_png())
-print("Decision Tree Created")
